18001044c7116efe17b877d31b60537147138301
Ritesh <ritesh@enablero.com>
2025-02-13 12:06:10 +0530

    fix: adjust minHeight for sign-in and sign-up components and remove gray text color from footer

--- app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -2,7 +2,7 @@ import { SignIn } from "@clerk/nextjs";
 
 export default function Page() {
     return (
-        <div className="flex items-center justify-center" style={{ minHeight: "calc(100vh - 200px)" }}>
+        <div className="flex items-center justify-center " style={{ minHeight: "calc(100vh - 50px)" }}>
             <SignIn />
         </div>
     )
--- components/common/footer.tsx
+++ components/common/footer.tsx
@@ -2,10 +2,10 @@
 const Footer = () => {
     return (
         <div className="flex flex-row justify-between px-4 py-2 border-t-2 text-black">
-            <p className="text-xs font-extralight text-gray-300">
+            <p className="text-xs font-extralight">
                 Copyright &copy; 2025 Mediboard. All rights reserved.
             </p>
-            <p className="text-xs font-extralight text-gray-300">
+            <p className="text-xs font-extralight">
                 Disclaimer: This is a record-keeping app only and does not provide medical advice or recommend any drugs.
             </p>
         </div>
